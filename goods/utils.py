324a4f03208a0251b9ca2c9d385ef9a0ece33fb7
Saikal Ulukbekova <ukyzsaikal@gmail.com>
2024-04-29 14:05:16 +0200

    Done with Simple search

--- goods/utils.py
+++ goods/utils.py
@@ -1,4 +1,13 @@
+from django.db.models import Q
 from goods.models import Products
 def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
+    keyword = [word for word in query.split() if len(word) > 2]
+
+    q_objects = Q()
+
+    for token in keyword:
+        q_objects |= Q(description__icontains=token)
+        q_objects |= Q(name__icaontains=token)
+    return Products.objects.filter(q_objects)
